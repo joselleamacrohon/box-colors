65d2197b040def18f949d83e13a14e69e13bc79d
joselleamacrohon <joselleamacrohon@gmail.com>
2018-11-06 14:06:41 +0800

    activity 1 on JS

---
+++ js/script.js
@@ -0,0 +1,110 @@
+let colorRed = document.getElementById("color1");
+let colorGreen = document.getElementById("color2");
+let colorBlue = document.getElementById("color3");
+let colorYellow = document.getElementById("color4");
+let colorPurple = document.getElementById("color5");
+let colorOrange = document.getElementById("color6");
+let colorPink = document.getElementById("color7");
+let colorAqua = document.getElementById("color8");
+let colorMaroon = document.getElementById("color9");
+let colorIndigo = document.getElementById("color10");
+let colorGold = document.getElementById("color11");
+let colorBlack = document.getElementById("color12");
+
+colorRed.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorRed.style.backgroundColor = "red";
+	colorRed.style.fontSize= "40px";
+	colorRed.style.color= "white";
+
+});
+
+colorGreen.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorGreen.style.backgroundColor = "green";
+	colorGreen.style.fontSize= "40px";
+	colorGreen.style.color= "white";
+
+});
+
+colorBlue.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorBlue.style.backgroundColor = "blue";
+	colorBlue.style.fontSize= "40px";
+	colorBlue.style.color= "white";
+
+});
+
+colorYellow.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorYellow.style.backgroundColor = "yellow";
+	colorYellow.style.fontSize= "40px";
+	colorYellow.style.color= "white";
+
+});
+
+
+colorPurple.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorPurple.style.backgroundColor = "purple";
+	colorPurple.style.fontSize= "40px";
+	colorPurple.style.color= "white";
+
+});
+
+colorOrange.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorOrange.style.backgroundColor = "orange";
+	colorOrange.style.fontSize= "40px";
+	colorOrange.style.color= "white";
+
+});
+
+colorPink.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorPink.style.backgroundColor = "pink";
+	colorPink.style.fontSize= "40px";
+	colorPink.style.color= "white";
+
+});
+
+colorAqua.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorAqua.style.backgroundColor = "aqua";
+	colorAqua.style.fontSize= "40px";
+	colorAqua.style.color= "white";
+
+});
+
+colorMaroon.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorMaroon.style.backgroundColor = "maroon";
+	colorMaroon.style.fontSize= "40px";
+	colorMaroon.style.color= "white";
+
+});
+
+colorIndigo.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorIndigo.style.backgroundColor = "indigo";
+	colorIndigo.style.fontSize= "40px";
+	colorIndigo.style.color= "white";
+
+});
+
+colorGold.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorGold.style.backgroundColor = "gold";
+	colorGold.style.fontSize= "40px";
+	colorGold.style.color= "white";
+
+});
+
+colorBlack.addEventListener ("click", function() {
+	//we want to change its font-size
+	colorBlack.style.backgroundColor = "black";
+	colorBlack.style.fontSize= "40px";
+	colorBlack.style.color= "white";
+
+});
+
